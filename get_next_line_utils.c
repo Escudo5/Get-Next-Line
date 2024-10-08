776fe9ac629a71bf7c32b07188535daf35308db2
Escudo5 <sergiomargar03@gmail.com>
2024-10-08 12:41:46 +0200

    utils y makefile

--- get_next_line_utils.c
+++ get_next_line_utils.c
@@ -6,7 +6,62 @@
 /*   By: smarquez <smarquez@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/07 11:14:09 by smarquez          #+#    #+#             */
-/*   Updated: 2024/10/07 11:21:37 by smarquez         ###   ########.fr       */
+/*   Updated: 2024/10/08 12:41:09 by smarquez         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+static char	*ft_strcat(char *dest, const char *src, int size)
+{
+	int	i;
+	int	j;
+
+	i = 0;
+	j = 0;
+	while (dest[i] != '\0')
+	{
+		i++;
+	}
+	while (j < size && src[j] != '\0')
+	{
+		dest[i] = src[j];
+		i++;
+		j++;
+	}
+	dest[i] = '\0';
+	return (dest);
+}
+
+char	*realloc_line_frag(char *line_frag, int new_size, int old_size)
+{
+	char	*new_line;
+	int		i;
+
+	new_line = malloc(new_size);
+	if (!new_line)
+		return (NULL);
+	i = 0;
+	while (i < old_size)
+	{
+		new_line[i] = line_frag[i];
+		i++;
+	}
+	new_line[i] = '\0';
+	free(line_frag);
+	return (new_line);
+}
+
+char	*ft_strlcpy(char *dest, const char *src, size_t size)
+{
+	size_t i;
+	if (size == 0)
+		;
+	return (ft_strlen(src));
+	i = 0;
+	while (i < size - 1 && src[i] != '\0')
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	dest[i] = '\0';
+	return (strlen(src));
+}
